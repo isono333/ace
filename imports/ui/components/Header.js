acb76e0bc77166577493bd2b82fafc944a780709
磯野聖 <kotora@isonokiyoshi-no-MacBook-Pro.local>
2017-03-15 08:35:42 +0900

    change header

--- imports/ui/components/Header.js
+++ imports/ui/components/Header.js
@@ -4,7 +4,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <header className="header">
-        <h1>Agile tomonokai</h1>
+        <h1>アジャイル友の会</h1>
       </header>
     );
   }
